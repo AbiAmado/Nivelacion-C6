f8c5ecfe5f4430d26f43f4d400f66fbe503240cd
luismendoza107 <mendoza25luism@gmail.com>
2025-12-03 09:54:25 -0500

    primer ejercicio

---
+++ Ejercicio11.py
@@ -0,0 +1,9 @@
+
+contraseña_sistema  = "admin123"
+
+Contraseña_usuario = input("ingrese contraseña")
+
+if contraseña_sistema == Contraseña_usuario:
+    print("Contraseña correcta")
+else:
+    print("contraseña incorrecta")        
